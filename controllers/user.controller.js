eebd43b1f375e3501f2be3385027539606bc3853
otherside9710 <jsarmientop.inca@gmail.com>
2019-03-28 17:16:23 -0500

    fix burrada

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,13 +3,13 @@ const userController = {};
 const User = require('../models/user');
 
 
-userController.getUsersByCredentials = (username, password) => {
+userController.getUsersByCredentials = async (username, password) => {
     try {
         const users = await User.findByEmailAndPassword(username, password);
         return users;
     } catch (err) {
         console.log({
-            status: err.code, 
+            status: err.code,
             message: err.message
         });
     }
@@ -54,10 +54,12 @@ userController.updateUSer = async (req, res) => {
     });
     try {
         //await Employee.findByIdAndUpdate(req.params.id, {$set: Employee}, {new: false});
-        const userFindOne = user.findByEmail
+        const userFindOne = user.findByEmail;
         await user.save();
         res.json({status:'200', message: 'User updated'});
     } catch (err) {
         res.json({status: err.code, message: err.message});
     }
 };
+
+module.exports = userController;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -11,28 +11,28 @@ router.put('/update', userController.updateUSer);
 
 //Esto para probar el jwt
 router.post('/doLogin', (req, res) => {
-	let username = req.body.user
-  	let password = req.body.password
-	
-	if( !(username === "test" && password==="1234")){
-	    res.status(401).send({
-	      error: 'usuario o contraseña inválidos'
-	    })
-	    return;
-  	}
+    let username = req.body.user
+    let password = req.body.password
 
-  	let tokenData = {
-	    username: username
-	    role: ["ROLE_USER", "ROLE_ADMIN"]
-  	}
-	 
-	let token = jwt.sign(tokenData, 'Secret Password', {
-	    expiresIn: 60 * 60 // expires in 1 hours
-	})
-	 
-	res.send({
-	  token
-	})
+    if (!(username === "test" && password === "1234")) {
+        res.status(401).send({
+            error: 'usuario o contraseña inválidos'
+        });
+        return;
+    }
+
+    let tokenData = {
+        username: username,
+        role: ["ROLE_USER", "ROLE_ADMIN"]
+    };
+
+    let token = jwt.sign(tokenData, 'Secret Password', {
+        expiresIn: 60 * 60 // expires in 1 hours
+    })
+
+    res.send({
+        token
+    })
 });
 
 module.exports = router;
