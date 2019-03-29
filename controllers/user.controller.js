01e66eb7cb3526c4c64eb856cb346bead37536d7
brayan26 <brayanpp2697@gmail.com>
2019-03-28 21:01:23 -0500

    autenticacion comletada

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,20 +1,43 @@
 const userController = {};
 
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 
-userController.getUsersByCredentials = async (email, password) => {
+userController.getUsersByCredentials = async (req, res) => {
     try {
-        //const users = await User.findByEmailAndPassword(username, password);
-        const users = await User.findByEmail(email);
-        return users;
+        let email = req.body.email;
+        let password = req.body.password;
+
+        console.log("searching: ",email , password);
+        let user = await User.find({"$and":[{"email":email}, {"password":password}]});
+
+        if (Object.keys(user).length > 0) {
+            let tokenData = {
+            username: email,
+            role: ["ROLE_USER", "ROLE_ADMIN"]
+            };
+
+            let token = jwt.sign(tokenData, 'Secret_Password', {
+                expiresIn: 60 * 60 // expires in 1 hours
+            });
+
+            res.send({
+                token
+            });
+        }
+        else {
+            res.status(401).send({
+                error: 'usuario o contraseña inválidos'
+            });
+        }
     } catch (err) {
-        console.error({
+        res.status(500).send({
             status: err.code,
             message: err.message
         });
     }
-    return false;
+    
 };
 
 
@@ -37,7 +60,7 @@ userController.createUSer = async (req, res) => {
         status: 'A'
     });
     try {
-        const userCreated = await User.create(user);
+        const userCreated = await user.save();
         res.json({status:'201', message: 'User created', entity: userCreated});
     } catch (err) {
         res.json({status: err.code, message: err.message});
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
 
 const userController = require('../controllers/user.controller');
 
@@ -10,30 +9,6 @@ router.post('/save', userController.createUSer);
 router.put('/update', userController.updateUSer);
 
 //Esto para probar el jwt
-router.post('/doLogin', (req, res) => {
-    let email = req.body.email;
-    let password = req.body.password;
-
-    (userController.getUsersByCredentials(email, password)).then( () => {
-    	res.status(401).send({
-            error: 'usuario o contraseña inválidos'
-        });
-        return;
-    })
-    .catch( () => {
-    	let tokenData = {
-        username: email,
-        role: ["ROLE_USER", "ROLE_ADMIN"]
-	    };
-
-	    let token = jwt.sign(tokenData, 'Secret Password', {
-	        expiresIn: 60 * 60 // expires in 1 hours
-	    });
-
-	    res.send({
-	        token
-	    })
-    })
-});
+router.post('/doLogin',  userController.getUsersByCredentials);
 
 module.exports = router;
