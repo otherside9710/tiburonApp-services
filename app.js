26077bca014d9d21c0a764a6870020e5c00e46bf
otherside9710 <jsarmientop.inca@gmail.com>
2019-04-01 20:58:50 -0500

    fix access allow from all origin

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('port', 3000);
 //Middleware
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(cors({origin : 'http://localhost:4200'}));
+app.use(cors());
 //Routes
 app.use('/api/tiburon-app', require('./routes/user.routes'));
 //Starting the the server
